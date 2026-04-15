9c405e6ad200b2e8145bc149ce7ba92b6d6c37f3
okirika26-droid <okirika26@gmail.com>
2026-04-15 01:45:14 +0100

    Fix import statements in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // كيعيط لملف App.tsx اللي غتكريي فيه الكود ديالك
-import './index.css';   // ملف التنسيقات ديالك
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
